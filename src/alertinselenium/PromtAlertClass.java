a84e1b2e993e8ac7431e16a0e66893afb6062abd
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-07-07 21:03:32 +0530

    updated code on 7 July

---
+++ src/alertinselenium/PromtAlertClass.java
@@ -0,0 +1,36 @@
+package alertinselenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PromtAlertClass {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\91992\\OneDrive\\Desktop\\chromedriver_win32 (1)\\chromedriver.exe");		
+		
+		   WebDriver driver = new ChromeDriver();
+		   driver.manage().window().maximize();
+		   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		   driver.get("https://demoqa.com/alerts");
+		   driver.findElement(By.id("promtButton")).click();
+		   
+		  
+		   
+		  Alert alt = driver.switchTo().alert();
+		 
+		  String altMesg = alt.getText();
+		  System.out.println(altMesg);
+		  
+		  alt.sendKeys("KTCTC");
+		  
+		  alt.accept();
+
+	}
+
+}
--- src/waitsinselenium/ImpliciteWaitClass.java
+++ src/waitsinselenium/ImpliciteWaitClass.java
@@ -13,7 +13,7 @@ public class ImpliciteWaitClass {
 		
 		   WebDriver driver = new ChromeDriver();
 		   driver.manage().window().maximize();
-		   driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		   
 		   driver.get("https://www.facebook.com/");	
 		   
